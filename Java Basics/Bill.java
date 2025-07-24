a75628848e1d08dbaf33dabb8de83c0e43647615
Jayesh Shivlani <jayeshshivlani23@outlook.com>
2025-07-24 21:52:50 +0530

    Day 11

---
+++ Java Basics/Bill.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Bill {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        float pen = sc.nextFloat();
+        float pencil = sc.nextFloat();
+        float eraser = sc.nextFloat();
+
+        float total = pen + pencil + eraser;
+        float tax = (18*total)/100;
+        float bill = total + tax;
+
+        System.out.println(bill);
+
+        sc.close();
+    }
+}
---
+++ Java Basics/average.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class average {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        int sum = A + B + C;
+        double average = sum / 3.0; // Here we need to write 3.0 instead of 3 otherwise it will only give the integer part 0f the calculation.
+        System.out.println(average);
+
+        sc.close();
+    }
+}
